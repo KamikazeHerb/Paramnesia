bc7beb40a6f6f17819abc5a468d56357fca0d117
KamikazeHerb <patrickodell572@gmail.com>
2021-09-28 20:57:53 +0100

    Migrated to standalone platforms

---
+++ Paramnesia/Assets/My Stuff/Scripts/InGameMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetSceneByName("Level2").IsValid() == true)
+        {
+            GameObject.FindGameObjectWithTag("NextLevelButton").SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ToggleMusic()
+    {
+        if (GameObject.FindGameObjectWithTag("GameController").GetComponent<GameMusic>().musicEnabled)
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameMusic>().musicEnabled = false;
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameMusic>().musicEnabled = true;
+        }
+    }
+}
